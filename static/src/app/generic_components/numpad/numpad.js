3e94d46c194d93e4bc9749f0dc91458e6878ce53
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-21 13:46:49 +0700

    FIX: numpad.js

--- static/src/app/generic_components/numpad/numpad.js
+++ static/src/app/generic_components/numpad/numpad.js
@@ -1,11 +1,16 @@
+import { patch } from "@web/core/utils/patch";
+import { Numpad } from "@point_of_sale/static/src/app/generic_components/numpad/numpad";
 import { BACKSPACE, ZERO, DECIMAL, getButtons } from "@point_of_sale/static/src/app/generic_components/numpad/numpad";
 
-export function enhancedButtons() {
-    // Mengganti 10, 20, 50 menjadi 10000, 20000, 50000
-    return getButtons([DECIMAL, ZERO, BACKSPACE], [
-        { value: "+10000" },
-        { value: "+20000" },
-        { value: "+50000" },
-        BACKSPACE,
-    ]);
-}
+// Custom override tombol kanan numpad
+patch(Numpad.prototype, {
+    get buttons() {
+        // Tombol kanan: +10000, +20000, +50000, backspace
+        return getButtons([DECIMAL, ZERO, BACKSPACE], [
+            { value: "+10000" },
+            { value: "+20000" },
+            { value: "+50000" },
+            BACKSPACE,
+        ]);
+    }
+});

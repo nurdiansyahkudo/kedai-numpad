d06546c3e8097cc8b915c94a44e04f35ab6c7308
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-21 16:09:21 +0700

    FIX: custom_numpad and custom_payment_screen

--- static/src/app/generic_components/numpad/custom_numpad.js
+++ static/src/app/generic_components/numpad/custom_numpad.js
@@ -1,28 +1,15 @@
 /** @odoo-module **/
 
+import { getButtons, BACKSPACE , Numpad} from "@point_of_sale/app/generic_components/numpad/numpad";
 import { patch } from "@web/core/utils/patch";
-import { enhancedButtons } from "@point_of_sale/app/generic_components/numpad/numpad";
+import { useService } from "@web/core/utils/hooks";
 
-patch(enhancedButtons, {
-    // Patch the function to override the default buttons
-    setup() {},
-    __call__: function () {
-        return [
-            { value: "1" },
-            { value: "2" },
-            { value: "3" },
-            { value: "4" },
-            { value: "5" },
-            { value: "6" },
-            { value: "7" },
-            { value: "8" },
-            { value: "9" },
-            { value: "0" },
-            { value: this.env.services.localization.decimalPoint },
-            { value: "Backspace", icon: "Delete" },
-            { value: "+10000" },
-            { value: "+20000" },
-            { value: "+50000" },
-        ];
-    },
-});
+// Fungsi override
+export function customEnhancedButtons() {
+    return getButtons(DEFAULT_LAST_ROW, [
+        { value: "+10000" },
+        { value: "+20000" },
+        { value: "+50000" },
+        BACKSPACE,
+    ]);
+}

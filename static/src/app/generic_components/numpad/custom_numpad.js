9d966702f358d8361a35f378aa235c7cf8944985
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-26 09:10:40 +0700

    FIX: Update numpad and payment screen

--- static/src/app/generic_components/numpad/custom_numpad.js
+++ static/src/app/generic_components/numpad/custom_numpad.js
@@ -1,17 +1,33 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import * as NumpadComp from "@point_of_sale/app/generic_components/numpad/numpad";
+import { Numpad, getButtons, DEFAULT_LAST_ROW, BACKSPACE } from "@point_of_sale/app/generic_components/numpad/numpad";
 
-// override enhancedButtons
-patch(NumpadComp, {
+patch(Numpad.prototype, {
+    setup() {
+        super.setup?.(); // if base has setup
+
+        if (!this.props.onClick) {
+            this.numberBuffer = this.env.services.number_buffer;
+            this.onClick = (buttonValue) => {
+                console.log("Clicked numpad button:", buttonValue);  // ✅ Log here
+                this.numberBuffer.sendKey(buttonValue);
+            };
+        } else {
+            this.onClick = this.props.onClick;
+        }
+    },
+});
+
+// patch enhancedButtons separately
+patch(Numpad, {
     enhancedButtons() {
         const customRightColumn = [
             { value: "10000", text: "10.000" },
             { value: "20000", text: "20.000" },
             { value: "50000", text: "50.000" },
-            NumpadComp.BACKSPACE,
+            BACKSPACE,
         ];
-        return NumpadComp.getButtons(NumpadComp.DEFAULT_LAST_ROW, customRightColumn);
+        return getButtons(DEFAULT_LAST_ROW, customRightColumn);
     },
 });

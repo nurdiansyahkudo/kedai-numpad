8253179ec01e01eace2eb3f4dca9219a0ebf40b7
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-26 09:24:19 +0700

    FIX: button 10k, 20k, 50k behavior

--- static/src/app/generic_components/numpad/custom_numpad.js
+++ static/src/app/generic_components/numpad/custom_numpad.js
@@ -1,17 +1,42 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import * as NumpadComp from "@point_of_sale/app/generic_components/numpad/numpad";
+import { Numpad, getButtons, DEFAULT_LAST_ROW, BACKSPACE } from "@point_of_sale/app/generic_components/numpad/numpad";
 
-// override enhancedButtons
-patch(NumpadComp, {
+patch(Numpad.prototype, {
+    setup() {
+        super.setup?.();
+        const isNominalButton = (val) => ["10000", "20000", "50000"].includes(val);
+
+        if (!this.props.onClick) {
+            this.numberBuffer = this.env.services.number_buffer;
+            this.pos = this.env.services.pos;
+
+            this.onClick = (buttonValue) => {
+                console.log("Clicked:", buttonValue);
+
+                if (isNominalButton(buttonValue)) {
+                    const paymentLine = this.pos.get_order().get_selected_paymentline();
+                    if (paymentLine) {
+                        paymentLine.set_amount(parseFloat(buttonValue));
+                        this.numberBuffer.set(buttonValue); // update buffer UI
+                    }
+                } else {
+                    this.numberBuffer.sendKey(buttonValue);
+                }
+            };
+        }
+    },
+});
+
+patch(Numpad, {
     enhancedButtons() {
         const customRightColumn = [
             { value: "10000", text: "10.000" },
             { value: "20000", text: "20.000" },
             { value: "50000", text: "50.000" },
-            NumpadComp.BACKSPACE,
+            BACKSPACE,
         ];
-        return NumpadComp.getButtons(NumpadComp.DEFAULT_LAST_ROW, customRightColumn);
+        return getButtons(DEFAULT_LAST_ROW, customRightColumn);
     },
 });

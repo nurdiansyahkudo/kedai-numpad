1303b06427d8abc64978c679036248d89608c143
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-28 14:45:05 +0700

    UPDATE: print sizing

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
         "point_of_sale._assets_pos": [
             "kedai_numpad/static/src/app/generic_components/numpad/custom_numpad.js",
             "kedai_numpad/static/src/app/screens/payment_screen/custom_payment_screen.js",
-            ("replace", "point_of_sale/static/src/css/pos_receipts.css", "kedai_numpad/static/src/css/pos_receipts.css"),
+            # ("replace", "point_of_sale/static/src/css/pos_receipts.css", "kedai_numpad/static/src/css/pos_receipts.css"),
         ],
     },
     "installable": True,

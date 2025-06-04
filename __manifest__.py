a8d5a40c2d187adaadeb59b733c6a07f2fd478e6
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-06-04 15:00:24 +0700

    UPDATE: payment screen numpad

--- __manifest__.py
+++ __manifest__.py
@@ -7,9 +7,7 @@
     "depends": ["point_of_sale"],
     'assets': {
         "point_of_sale._assets_pos": [
-            # "kedai_numpad/static/src/app/generic_components/numpad/custom_numpad.js",
             "kedai_numpad/static/src/app/screens/payment_screen/custom_payment_screen.js",
-            # ("replace", "point_of_sale/static/src/css/pos_receipts.css", "kedai_numpad/static/src/css/pos_receipts.css"),
         ],
     },
     "installable": True,

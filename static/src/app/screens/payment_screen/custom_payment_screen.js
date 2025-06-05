4899ad3a600c2c19d6e344356a15217acfa790b3
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-06-05 11:29:15 +0700

    UPDATE: payment screen button

--- static/src/app/screens/payment_screen/custom_payment_screen.js
+++ static/src/app/screens/payment_screen/custom_payment_screen.js
@@ -4,23 +4,20 @@ import { patch } from "@web/core/utils/patch";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { enhancedButtons } from "@point_of_sale/app/generic_components/numpad/numpad";
 
-// 1. Tambahkan tombol nominal di PaymentScreen
 patch(PaymentScreen.prototype, {
     getNumpadButtons() {
-        console.log("getNumpadButton called");
-        
         const colorClassMap = {
             "10000": "o_colorlist_item_color_transparent_10",
             "20000": "o_colorlist_item_color_transparent_10",
             "50000": "o_colorlist_item_color_transparent_10",
             "Backspace": "o_colorlist_item_color_transparent_1",
-            ".": "o_colorlist_item_color_transparent_6",
+            ".": "o_colorlist_item_color_transparent_6", // localization.decimalPoint
             "-": "o_colorlist_item_color_transparent_3",
         };
 
         return enhancedButtons().map((button) => ({
             ...button,
-            class: `${colorClassMap[button.value] || ""}`,
+            class: colorClassMap[button.value] || "",
         }));
     },
 });

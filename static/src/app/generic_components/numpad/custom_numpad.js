cfc09de15a45717261f544ccdaa48c551d111b48
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-06-05 11:31:44 +0700

    UPDATE: payment screen button

--- static/src/app/generic_components/numpad/custom_numpad.js
+++ static/src/app/generic_components/numpad/custom_numpad.js
@@ -4,19 +4,6 @@ import { patch } from "@web/core/utils/patch";
 import { useService } from "@web/core/utils/hooks";
 import * as NumpadComp from "@point_of_sale/app/generic_components/numpad/numpad";
 
-// 1. Override tombol
-patch(NumpadComp, {
-    enhancedButtons() {
-        const customRightColumn = [
-            { value: "10000", text: "10.000" },
-            { value: "20000", text: "20.000" },
-            { value: "50000", text: "50.000" },
-            NumpadComp.BACKSPACE,
-        ];
-        return NumpadComp.getButtons(NumpadComp.DEFAULT_LAST_ROW, customRightColumn);
-    },
-});
-
 // Override logika klik tombol
 patch(NumpadComp.Numpad.prototype, {
     setup() {
--- static/src/app/screens/payment_screen/custom_payment_screen.js
+++ static/src/app/screens/payment_screen/custom_payment_screen.js
@@ -2,22 +2,35 @@
 
 import { patch } from "@web/core/utils/patch";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
-import { enhancedButtons } from "@point_of_sale/app/generic_components/numpad/numpad";
+import * as NumpadComp from "@point_of_sale/app/generic_components/numpad/numpad";
+import { useService } from "@web/core/utils/hooks";
 
+// 1. Tambahkan tombol nominal di PaymentScreen
 patch(PaymentScreen.prototype, {
     getNumpadButtons() {
+        console.log("getNumpadButton called");
+        
         const colorClassMap = {
             "10000": "o_colorlist_item_color_transparent_10",
             "20000": "o_colorlist_item_color_transparent_10",
             "50000": "o_colorlist_item_color_transparent_10",
             "Backspace": "o_colorlist_item_color_transparent_1",
-            ".": "o_colorlist_item_color_transparent_6", // localization.decimalPoint
+            ".": "o_colorlist_item_color_transparent_6",
             "-": "o_colorlist_item_color_transparent_3",
         };
 
-        return enhancedButtons().map((button) => ({
+        const customRightColumn = [
+            { value: "10000", text: "10.000" },
+            { value: "20000", text: "20.000" },
+            { value: "50000", text: "50.000" },
+            NumpadComp.BACKSPACE,
+        ];
+
+        const buttons = NumpadComp.getButtons(NumpadComp.DEFAULT_LAST_ROW, customRightColumn);
+
+        return buttons.map((button) => ({
             ...button,
-            class: colorClassMap[button.value] || "",
+            class: `${colorClassMap[button.value] || ""}`,
         }));
     },
 });
